af617c468e425eab84892b4d649e12e6c7693b01
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-23 10:23:59 +0000

    bh

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,14 +1,13 @@
-// Lire deux nombres et les inverser sans utiliser de variable temporaire.
 #include <stdio.h>
 int main() {
     int a, b;
-    // Lecture des valeurs
+    // Lecture des valeurs.
     scanf("%d %d", &a, &b);
-    // Échange sans variable temporaire
+    // Échange sans variable temporaire.
     a = a + b;
     b = a - b;
     a = a - b;
-    // Affichage du résultat
-    printf("Apres echange: a = %d, b = %d", a, b);
+    // Affichage du résultat.
+    printf("Après échange: a = %d, b = %d", a, b);
     return 0;
 }
